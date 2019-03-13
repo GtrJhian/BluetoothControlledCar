556e4cc7404b98256966fe3e457af58d6491c7c5
Zildjian Mergilla <gtrmergillazildjianl@gmail.com>
2019-03-13 19:24:08 +0800

    Release

--- src/Config.h
+++ src/Config.h
@@ -2,9 +2,11 @@
 
 #pragma region  SERIAL_DEBUG_CONFIG
 
-    #define SERIAL_DEBUG true
+    #define SERIAL_DEBUG false
     #define SERIAL_BAUD_RATE 9600
     #define LINE Serial.println("===========================================");
+    #define POLL_BYTE 0x69
+    #define REQUEST_DELAY 100
 
 #pragma endregion
 
@@ -12,16 +14,16 @@
 #pragma region BLUETOOTH_CONFIG
 
     #define BLUETOOTH_BAUD_RATE 9600
-    #define BLUETOOTH_TX_PIN 0
-    #define BLUETOOTH_RX_PIN 0
+    #define BLUETOOTH_TX_PIN 12
+    #define BLUETOOTH_RX_PIN 11
 
 #pragma endregion
 
 #pragma region MOTOR_PINOUT_CONFIG
 
-    #define MOTOR_1A_PIN 0
-    #define MOTOR_1B_PIN 0
-    #define MOTOR_2A_PIN 0
-    #define MOTOR_2B_PIN 0
+    #define MOTOR_1A_PIN 10
+    #define MOTOR_1B_PIN 9
+    #define MOTOR_2A_PIN 6
+    #define MOTOR_2B_PIN 5
 
 #pragma endregion
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,23 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 #include "Config.h"
+#include "L293D.h"
+
+#pragma region GlobalVariables
+
+SoftwareSerial BL(BLUETOOTH_TX_PIN, BLUETOOTH_RX_PIN);
+MotorDriver *motorDriver;
+
+#pragma endregion
+
+void scale(int16_t &x){
+  if(x & 0x80){
+    x &= 0x7F;    
+    x = -x;
+  }
+  x = map(x, -101, 100 , -256, 255);
+}
+
 void setup() {
   // put your setup code here, to run once:
   #if SERIAL_DEBUG==true
@@ -8,9 +26,29 @@ void setup() {
     Serial.println("Serial Debug Enabled");
     LINE
   #endif
-
+  BL.begin(9600);
+  motorDriver = new MotorDriver(MOTOR_1A_PIN, MOTOR_1B_PIN, MOTOR_2A_PIN, MOTOR_2B_PIN);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+    if(BL.available()){
+      int16_t left = BL.read();
+      int16_t right = BL.read();
+      scale(left);
+      scale(right);
+      #if SERIAL_DEBUG==true
+        Serial.print("x: ");
+        Serial.print(left,DEC);
+        Serial.print(" y: ");
+        Serial.println(right,DEC);
+      #endif
+        motorDriver->driveLeft(left)->driveRight(right);
+      
+    }
+    else{
+       BL.write(POLL_BYTE);
+       delay(REQUEST_DELAY);
+    }
 }
+
